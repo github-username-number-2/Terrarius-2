455d8cfc0f6456f509582c1883e0a61a74f5a5f1
Luke Baja-Ricketts <luekbaja@gmail.com>
2025-01-04 15:18:40 -0500

    nuke

--- src/renderer.rs
+++
@@ -1,5 +0,0 @@
-pub struct Renderer;
-
-impl Renderer {
-    pub const BLOCK_PIXEL_COUNT: f32 = 30.0;
-}
--- src/world/block_data.rs
+++
@@ -1,8 +0,0 @@
-use crate::world::block_textures::BlockTypes;
-
-pub const SOLIDS: [u16; 4] = [
-    BlockTypes::Grass as u16,
-    BlockTypes::Dirt as u16,
-    BlockTypes::Stone as u16,
-    BlockTypes::Planks as u16,
-];
